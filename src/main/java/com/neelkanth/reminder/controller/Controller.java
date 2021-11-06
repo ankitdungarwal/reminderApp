ce3050c6daa72a9b4a495d67341939b8d81f0331
ankit dungarwal <ankitdungarwal92@gmail.com>
2021-11-06 12:30:12 +0530

    minor update for database connectivity

--- src/main/java/com/neelkanth/reminder/controller/Controller.java
+++ src/main/java/com/neelkanth/reminder/controller/Controller.java
@@ -4,7 +4,6 @@ package com.neelkanth.reminder.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
