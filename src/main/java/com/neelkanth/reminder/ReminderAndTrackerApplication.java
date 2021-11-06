b4259976ea8276381949899037201fbdf2e2795f
ankit dungarwal <ankitdungarwal92@gmail.com>
2021-11-06 07:41:33 +0530

    initial setup

---
+++ src/main/java/com/neelkanth/reminder/ReminderAndTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.neelkanth.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderAndTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderAndTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neelkanth/reminder/ReminderAndTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neelkanth.reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderAndTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
